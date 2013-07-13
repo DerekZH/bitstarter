f8f516d66f2c409b792ca192aa527c1c5aa9ec32
DerekZH <zhoudx.dd@gmail.com>
2013-07-13 15:15:12 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var index_html = fs.readFileSync('index.html','utf8');
+var buffer = new buffer(60);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-  response.send(index_html.toString('utf-8'));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
